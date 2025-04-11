b51fb3132a2e9f4540902e7ccd65291d29648759
darknight-123 <81505859+darknight-123@users.noreply.github.com>
2025-04-12 00:57:58 +0800

    Update index.php

--- graduate_website/index.php
+++ graduate_website/index.php
@@ -1,23 +1,24 @@
 <!DOCTYPE html>
 <?php
     session_start();
-    if(!empty($_POST['Send']) && !empty($_POST['Username'])&& !empty($_POST['Password']))
+    if (!empty ($_POST['Send']) && !empty ($_POST['Username']) && !empty ($_POST['Password']))
     {      
         require_once "./website_1/Login.php";
     }
 ?>
+	    
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-     <meta name=”viewport” content="width=device-width, user-scalable=yes, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name=”viewport” content="width=device-width, user-scalable=yes, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <title>登入介面</title>
 
 </head>
 <style>
  html {
             height: 100%;
-        }
+}
 body {
             background: "./icon/farm.jpg";
             background-repeat: no-repeat;
@@ -51,28 +52,20 @@ body {
 
 <?php
     
-		if(!empty($_POST["Send"]) and $_POST["Send"]=="登入")
-		{
-			if(!empty($_POST['Result']) and $_POST['Result']=="success")
-			{
-				$_SESSION['Username']=$_POST['Username'];
-				header("Location: ./website_1/Day/Graph.php"); 
-				print_r("登入成功");
-			}
-			else if(!empty($_POST['Result']) and $_POST['Result']=="failure")
-			{
-				echo  '<span style="color:red;font-size: 40px;"> ' . "帳號或密碼錯誤" . '<script>alert("帳號或密碼錯誤")</script>';
-			}
-			
-			else
-			{
-				echo '<span style="color:red;font-size: 40px;"> '. "帳號或密碼不可為空白" . '<script>alert("帳號或密碼不可為空白")</script>';
-			}
+        if (!empty($_POST["Send"]) and $_POST["Send"]=="登入") {
+	     if (!empty($_POST['Result']) and $_POST['Result']=="success") {
+			$_SESSION['Username']=$_POST['Username'];
+			header("Location: ./website_1/Day/Graph.php"); 
+			print_r("登入成功");
 		}
+		else if (!empty($_POST['Result']) and $_POST['Result']=="failure") {
+			echo  '<span style="color:red;font-size: 40px;"> ' . "帳號或密碼錯誤" . '<script>alert("帳號或密碼錯誤")</script>';
+		}
+		else {
+			echo '<span style="color:red;font-size: 40px;"> '. "帳號或密碼不可為空白" . '<script>alert("帳號或密碼不可為空白")</script>';
+		}
+	}
 ?>
-
-
-
 
 
 </body>
