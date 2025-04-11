abacc36bfad1ddc2af4a5837049e09c5590722b0
darknight-123 <81505859+darknight-123@users.noreply.github.com>
2025-04-12 01:16:57 +0800

    Update Tempature.php

--- graduate_website/website_1/Day/Tempature.php
+++ graduate_website/website_1/Day/Tempature.php
@@ -1,24 +1,20 @@
 <?php
-print($_POST['Tempature_ID']);
-	if(isset($_POST['Tempature_ID']))
+print ($_POST['Tempature_ID']);
+	if (isset ($_POST['Tempature_ID']))
 	{
-		
 		require_once "../conn.php";
-		$RaspberryID=$_POST['Tempature_ID'];
+		$RaspberryID = $_POST['Tempature_ID'];
 		//尋找最小日期
 		
 		//搜尋結果
-		if(!empty($_POST['Day']))
+		if (!empty ($_POST['Day']))
 		{
-			$Day=$_POST['Day'];
-			
-			$sql="select * from location_tempature where RaspberryID='$RaspberryID' and Day='$Day'";
-			$result=$conn->query($sql);
+			$Day    = $_POST['Day'];
+			$sql    = "select * from location_tempature where RaspberryID = '$RaspberryID' and Day = '$Day'";
+			$result = $conn->query($sql);
 			while ($row = $result->fetch_assoc()) {
-				
 				$location_tempature[]=$row;
 			}
-			
 		}
 	}
 ?>
