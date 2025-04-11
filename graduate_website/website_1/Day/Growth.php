78437972f7c4d13f6e4a6bd8b60df68c53e71bb6
darknight-123 <81505859+darknight-123@users.noreply.github.com>
2025-04-12 01:07:32 +0800

    Update Growth.php

--- graduate_website/website_1/Day/Growth.php
+++ graduate_website/website_1/Day/Growth.php
@@ -1,20 +1,17 @@
 <?php
-if(isset($_POST['ID']) and isset($_POST['Day']))
+if (isset($_POST['ID']) and isset($_POST['Day']))
 {
 	require_once "../conn.php";
-	$ID=$_POST['ID'];
-	$Day=$_POST['Day'];
+	$ID  = $_POST['ID'];
+	$Day = $_POST['Day'];	
+	$sql = "select * from tomato_growth where RaspberryID = '$ID' and Day = '$Day'";
 	
-	$sql="select * from tomato_growth where RaspberryID='$ID' and Day='$Day'";
-	
-	$result=$conn->query($sql);
-	$row = $result->fetch_assoc();
-	if(!empty($row))
-	{
-	$_POST['Growth']=$row['Growth'];
+	$result = $conn->query ($sql);
+	$row    = $result->fetch_assoc();
+	if (!empty ($row)) {
+		$_POST['Growth'] = $row['Growth'];
 	}
-	else
-	{
+	else {
 		$_POST['Growth']='無資料';
 	}
 }
