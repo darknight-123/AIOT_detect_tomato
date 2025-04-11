81b69f6aa866217341abc92af52c00758a083c8b
darknight-123 <81505859+darknight-123@users.noreply.github.com>
2025-04-12 01:14:56 +0800

    Update Small_date.php

--- graduate_website/website_1/Day/Small_date.php
+++ graduate_website/website_1/Day/Small_date.php
@@ -1,22 +1,17 @@
 <?php
 require_once "../conn.php";
-foreach($list as $key => $value)
-		{
-			$ID=$value['RaspberryID'];
-			
-			$sql="select * from conditions where RaspberryID=$ID";
-			$result=$conn->query($sql);
-			$row = $result->fetch_assoc();
-			if(!empty($row['Day'])and (empty($Inital_Day) or strtotime($Inital_Day)>strtotime($row['Day'])))
-			{
-				$Inital_Day=$row['Day'];
-				$Final_Day=$row['Day'];
-			}
-			while ($row = $result->fetch_assoc()) {
-				if(strtotime($Final_Day)<strtotime($row['Day']))
-				$Final_Day=$row['Day'];
-				}
-		
-			
-		}
+foreach ($list as $key => $value) {
+	$ID     = $value['RaspberryID'];			
+	$sql    = "select * from conditions where RaspberryID=$ID";
+	$result = $conn->query($sql);
+	$row    = $result->fetch_assoc();
+	if (!empty ($row['Day']) and (empty($Inital_Day) or strtotime($Inital_Day) > strtotime ($row['Day']))) {
+		$Inital_Day = $row['Day'];
+		$Final_Day  = $row['Day'];
+	}
+	while ($row = $result->fetch_assoc()) {
+		if (strtotime ($Final_Day) < strtotime ($row['Day']))
+			$Final_Day = $row['Day'];
+	}
+}
 ?>
